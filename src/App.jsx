a3d526edbda030529518e5ebe680913974103d60
RagiliawanPutraa <ragiliawanputraa@gmail.com>
2025-05-30 22:49:00 +0700

    flow mix file

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,14 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Sentiment from './pages/sentiment';
 import Login from "./pages/login";
-import Flow from "./pages/flow";
 import Register from "./pages/Register";
+import Flow from "./pages/flow";
+import DashboardContent from "./pages/flow/DashboardContent";
+import AnalysisContent from "./pages/flow/AnalysisContent";
+import AnalysisResults from "./pages/flow/AnalysisResults";
+import HistoryContent from "./pages/flow/HistoryContent";
+import ReportsContent from "./pages/flow/ReportsContent";
+import SettingsContent from "./pages/flow/SettingsContent";
 import './app.css';
 import './index.css';
 
@@ -16,7 +22,15 @@ function App() {
       <Route path="/sentiment" element={<Sentiment />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/flow" element={<Flow />} />
+      {/* Nested route for /flow */}
+      <Route path="/flow" element={<Flow />}>
+        <Route index element={<DashboardContent />} />
+        <Route path="analysis" element={<AnalysisContent />} />
+        <Route path="analysis/results" element={<AnalysisResults />} />
+        <Route path="history" element={<HistoryContent />} />
+        <Route path="reports" element={<ReportsContent />} />
+        <Route path="settings" element={<SettingsContent />} />
+      </Route>
     </Routes>
   );
 }
